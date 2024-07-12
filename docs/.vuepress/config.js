441c25ae8227ff4c5231dae573e8ad0b109c5f71
wangzhihui <2878459042@qq.com>
2024-07-12 09:57:40 +0800

    init commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: "Daily Question Docs",
+  description: "Front End Developer Daily Question",
+  themeConfig: {
+    theme: 'reco',
+    locales: {
+      '/': {
+        lang: 'zh-CN'
+      }
+    },
+    themeConfig: {
+      subSidebar: 'auto'
+    },
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "Missy Blog",
+        items: [
+          { text: "Github", link: "https://github.com/AnAiYi" },
+          {
+            text: "掘金",
+            link: "https://juejin.cn/user/1856437358176254",
+          },
+          {
+            text: "Life",
+            link:"https://anaiyi.biebio.com/"
+          }
+        ],
+      },
+    ],
+    sidebar: [
+      {
+          title: '欢迎学习',
+          path: '/',
+          collapsable: false, // 不折叠
+          children: [
+              { title: "学前必读", path: "/" }
+          ]
+      },
+      {
+        title: "基础学习",
+        path: '/handbook/ConditionalTypes',
+        collapsable: false, // 不折叠
+        children: [
+          { title: "条件类型", path: "/handbook/ConditionalTypes" },
+          { title: "泛型", path: "/handbook/Generics" }
+        ],
+      }
+    ]
+  }
+};
